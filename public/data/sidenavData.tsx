2081d3c3cf5bcc2616e3ed8543318384b31e05e6
Ting Yun <ting.yun.career@gmail.com>
2024-05-07 20:51:46 -0700

    add side nav type to silence tslint

--- public/data/sidenavData.tsx
+++ public/data/sidenavData.tsx
@@ -1,6 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 
-export const sidenavData = [
+interface SidenavItem {
+  id: number;
+  name: string;
+  url?: string;
+  icon: string;
+  submenus?: {
+    title: string;
+    url: string;
+  }[];
+}
+
+export const sidenavData: SidenavItem[] = [
   {
     id: 456001,
     name: "Dashboard",
@@ -19,64 +30,64 @@ export const sidenavData = [
     url: "/prices",
     icon: "payments",
   },
-  // {
-  //   id: 456004,
-  //   name: "Wallets",
-  //   url: "/wallets",
-  //   icon: "account_balance_wallet",
-  // },
-  // {
-  //   id: 456005,
-  //   name: "Promotions",
-  //   url: "/promotions",
-  //   icon: "percent",
-  // },
-  // {
-  //   id: 456006,
-  //   name: "Activities",
-  //   url: "/activities",
-  //   icon: "insights",
-  // },
-  // {
-  //   id: 456007,
-  //   name: "Notifications",
-  //   url: "/notifications",
-  //   icon: "notifications",
-  // },
-  // {
-  //   id: 456008,
-  //   name: "Settings",
-  //   url: "/settings",
-  //   icon: "settings",
-  // },
-  // {
-  //   id: 456009,
-  //   name: "Utilities",
-  //   icon: "construction",
-  //   submenus: [
-  //     {
-  //       title: "Protected Page",
-  //       url: "/utillity/protected",
-  //     },
-  //     {
-  //       title: "Error Page",
-  //       url: "/utillity/error",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 4560010,
-  //   name: "Authentication",
-  //   icon: "work",
-  //   submenus: [
-  //     {
-  //       title: "Sign In",
-  //       url: "/utillity/sign_in",
-  //     },
-  //     {
-  //       title: "Sign Up",
-  //       url: "/utillity/sign_up",
-  //     },
-  //   ],
-  // },
+  {
+    id: 456004,
+    name: "Wallets",
+    url: "/wallets",
+    icon: "account_balance_wallet",
+  },
+  {
+    id: 456005,
+    name: "Promotions",
+    url: "/promotions",
+    icon: "percent",
+  },
+  {
+    id: 456006,
+    name: "Activities",
+    url: "/activities",
+    icon: "insights",
+  },
+  {
+    id: 456007,
+    name: "Notifications",
+    url: "/notifications",
+    icon: "notifications",
+  },
+  {
+    id: 456008,
+    name: "Settings",
+    url: "/settings",
+    icon: "settings",
+  },
+  {
+    id: 456009,
+    name: "Utilities",
+    icon: "construction",
+    submenus: [
+      {
+        title: "Protected Page",
+        url: "/utillity/protected",
+      },
+      {
+        title: "Error Page",
+        url: "/utillity/error",
+      },
+    ],
+  },
+  {
+    id: 4560010,
+    name: "Authentication",
+    icon: "work",
+    submenus: [
+      {
+        title: "Sign In",
+        url: "/utillity/sign_in",
+      },
+      {
+        title: "Sign Up",
+        url: "/utillity/sign_up",
+      },
+    ],
+  },
 ];
