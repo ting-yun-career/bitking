a1f65bf4f090e75dcc43f2c0d7e7c38c4c5deda0
Ting Yun <ting.yun.career@gmail.com>
2024-05-09 22:06:01 -0700

    credit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,18 +4,19 @@ const Footer = () => {
   return (
     <footer className="bg-Primary-bg px-2 lg:px-6 pb-6 text-center ">
       <hr className="mb-3 lg:mb-6 border-Neutral-10" />
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-between items-center gap-2">
-        <p className="lg:text-start text-sm md:text-lg text-white order-last lg:order-first ">
-          Designed by
-          <Link className="text-Neutral-8 ml-[6px]" target="_blank"
-            href="https://themeforest.net/item/cryptdash-crypto-exchange-dashboard-html-template/46092301">
-            UIAxis
-          </Link>, Customized by
-          <Link href="#" className="text-Neutral-8 ml-[6px]">
-            {process.env.author}
-          </Link>
-        </p>
-      </div>
+      <p className="lg:text-start text-sm md:text-lg text-white order-last lg:order-first ">
+        Designed by
+        <Link className="text-Neutral-8 ml-[6px]" target="_blank"
+          href="https://themeforest.net/item/cryptdash-crypto-exchange-dashboard-html-template/46092301">
+          UIAxis
+        </Link>, Customized by
+        <Link href="#" className="text-Neutral-8 ml-[6px]">
+          {process.env.author}
+        </Link>, Crypto Icons by<Link className="text-Neutral-8 ml-[6px]" target="_blank"
+          href="https://github.com/spothq/cryptocurrency-icons/tree/master">
+          Spothq
+        </Link>
+      </p>
     </footer>
   );
 };
