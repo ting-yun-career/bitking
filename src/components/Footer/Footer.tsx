afd62ed83c2a9f82eaced02b34e1808dba880eda
Ting Yun <ting.yun.career@gmail.com>
2024-05-25 19:19:51 -0700

    credit gone

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,16 +5,9 @@ const Footer = () => {
     <footer className="bg-Primary-bg px-2 lg:px-6 pb-6 text-center ">
       <hr className="mb-3 lg:mb-6 border-Neutral-10" />
       <p className="lg:text-start text-sm md:text-lg text-white order-last lg:order-first ">
-        Designed by
-        <Link className="text-Neutral-8 ml-[6px]" target="_blank"
-          href="https://themeforest.net/item/cryptdash-crypto-exchange-dashboard-html-template/46092301">
-          UIAxis
-        </Link>, Customized by
+        Made by
         <Link href="#" className="text-Neutral-8 ml-[6px]">
           {process.env.author}
-        </Link>, Crypto Icons by<Link className="text-Neutral-8 ml-[6px]" target="_blank"
-          href="https://github.com/spothq/cryptocurrency-icons/tree/master">
-          Spothq
         </Link>
       </p>
     </footer>
