dc89316546f74f69348a291f0db00bb3e53961f9
Ting Yun <ting.yun.career@gmail.com>
2024-06-04 06:18:51 -0700

    market trades

--- src/components/Exchange/MarketTrades.jsx
+++ src/components/Exchange/MarketTrades.jsx
@@ -6,114 +6,45 @@ const MarketTrades = () => {
   return (
     <div className="p-4 lg:px-7 lg:py-6 rounded-xl col-span-12 xl:col-span-8">
       <div>
-        <Tab.Group>
-          <div className="flex flex-wrap gap-2 justify-between items-center">
-            <h5 className="text-xl font-semibold text-Neutral-6 leading-[26px]">
-              Market Trades
-            </h5>
-            <Tab.List>
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={
-                      selected
-                        ? "text-[#F8FAFC] bg-gradient-to-r from-[#33AB71] to-[#06753F] text-xs leading-[18px] px-3 py-1 rounded outline-none mr-4 border border-Neutral-7"
-                        : "text-Neutral-6 text-xs leading-[18px] px-3 py-1 rounded outline-none mr-4 border border-Neutral-7"
-                    }
-                  >
-                    Price Chart
-                  </button>
-                )}
-              </Tab>
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={
-                      selected
-                        ? "text-[#F8FAFC] bg-gradient-to-r from-[#33AB71] to-[#06753F] text-xs leading-[18px] px-3 py-1 rounded outline-none border border-Neutral-7"
-                        : "text-Neutral-6 text-xs leading-[18px] px-3 py-1 rounded outline-none border border-Neutral-7"
-                    }
-                  >
-                    Deep Chart
-                  </button>
-                )}
-              </Tab>
-            </Tab.List>
-          </div>
-          <hr className="border-Neutral-10 my-5" />
-          <Tab.Panels>
-            <Tab.Panel>
-              <div className="overflow-x-auto">
-                <table className="table whitespace-nowrap">
-                  <thead>
-                    <tr className="border-Neutral-7 text-base font-bold text-Neutral-6 leading-[24px]">
-                      <th>Time</th>
-                      <th>Price (USDT)</th>
-                      <th>Amount (BTC)</th>
-                      <th>Total (USDT)</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {priceTrades.map((trade) => (
-                      <tr
-                        key={trade.id}
-                        className="border-Neutral-7 text-base text-Neutral-6 leading-[24px]"
-                      >
-                        <td>
-                          <div>{trade.time}</div>
-                        </td>
-                        <td>
-                          <div>{trade.Price}</div>
-                        </td>
-                        <td>
-                          <div>{trade.amount}</div>
-                        </td>
-                        <td>
-                          <div>{trade.total}</div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </Tab.Panel>
-            <Tab.Panel>
-              <div className="overflow-x-auto">
-                <table className="table whitespace-nowrap">
-                  <thead>
-                    <tr className="border-Neutral-7 text-base font-bold text-Neutral-6 leading-[24px]">
-                      <th>Time</th>
-                      <th>Price (USDT)</th>
-                      <th>Amount (BTC)</th>
-                      <th>Total (USDT)</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {deepTrades.map((trade) => (
-                      <tr
-                        key={trade.id}
-                        className="border-Neutral-7 text-base text-Neutral-6 leading-[24px]"
-                      >
-                        <td>
-                          <div>{trade.time}</div>
-                        </td>
-                        <td>
-                          <div>{trade.Price}</div>
-                        </td>
-                        <td>
-                          <div>{trade.amount}</div>
-                        </td>
-                        <td>
-                          <div>{trade.total}</div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+        <div className="flex flex-wrap gap-2 justify-between items-center">
+          <h5 className="text-xl font-semibold text-Neutral-6 leading-[26px]">
+            Market Trades
+          </h5>
+        </div>
+        <hr className="border-Neutral-10 my-5" />
+        <div className="overflow-x-auto">
+          <table className="table whitespace-nowrap">
+            <thead>
+              <tr className="border-Neutral-7 text-base font-bold text-Neutral-6 leading-[24px]">
+                <th>Time</th>
+                <th>Price (USDT)</th>
+                <th>Amount (BTC)</th>
+                <th>Total (USDT)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {priceTrades.map((trade) => (
+                <tr
+                  key={trade.id}
+                  className="border-Neutral-7 text-base text-Neutral-6 leading-[24px]"
+                >
+                  <td>
+                    <div>{trade.time}</div>
+                  </td>
+                  <td>
+                    <div>{trade.Price}</div>
+                  </td>
+                  <td>
+                    <div>{trade.amount}</div>
+                  </td>
+                  <td>
+                    <div>{trade.total}</div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
